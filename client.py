9dd903e68fe1d8ddd8744ae38b3ab4741388c02d
Jeremy Geslin <jeremy.geslin@nxp.com>
2020-02-14 08:13:43 +0000

    Fix connection with server

--- client.py
+++ client.py
@@ -6,24 +6,26 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 5010
 BUFFER_SIZE = 1024
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-
 p = pexpect.spawn('/home/pi/linux_libnfc-nci/nfcDemoApp poll', timeout=None)
+
 for line in p:
 	if "Text :" in line:
+		print "Scan success"
+		print "Connecting to TCP server"
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((TCP_IP, TCP_PORT))
-		print "scan success"
-		test = line.strip()
-		test = test.replace('Text :','')
-		test = test.replace('	','')
-		test = test.replace(' ','')
-		test = test.replace("'",'')
-		print test
-		s.send(test)
-		data = s.recv(BUFFER_SIZE)
-		print "received data:", data
+		print "Connected to server"
+		NfcContent = line.strip()
+		NfcContent = NfcContent.replace('Text :','')
+		NfcContent = NfcContent.replace('	','')
+		NfcContent = NfcContent.replace(' ','')
+		NfcContent = NfcContent.replace("'",'')
+		print "Sending \"" + NfcContent + "\" to server"
+		s.send(NfcContent)
+		serverData = s.recv(BUFFER_SIZE)
+		print "Server echo:", serverData
+		s.close()
+		print "Connection to server closed"
 	#if  "Lost" in line:
 		#print "lost"
 		#s.send("lost")
